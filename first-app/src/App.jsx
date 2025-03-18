c2744268f44790ca9460bde06eb0c4d04ded6a77
Angelika Ervasti <angelika.ervasti@hotmail.fi>
2025-03-19 00:29:11 +0200

    Updates

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -1,14 +1,37 @@
+import { useState } from 'react';
+
 import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js'
 
 function App() {
+  const [ selectedTopic, setSelectedTopic ] = useState(null);
+
+
   function handleSelect(selectedButton) {
     //selectedButton => 'components', 'jsx' etc
-    console.log(selectedButton);
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
 
   }
+
+  console.log('APP COMPONENT EXECUTING');
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (<div id="tab-content">
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>{EXAMPLES[selectedTopic].code}</code>
+      </pre>
+    </div>);
+  }
+
+
   return (
     <div>
       <Header />
@@ -31,7 +54,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('components')}>State</TabButton>
             
           </menu>
-          Dynamic content
+          {tabContent}
 
         </section>
       </main>
