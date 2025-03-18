5c92572895d3b4fd5eaa7e2cec01b9153e2c7acc
Angelika Ervasti <angelika.ervasti@hotmail.fi>
2025-03-18 22:30:36 +0200

    Functions

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
+import compononetsImg from './assets/components.png';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -10,7 +11,7 @@ function Header() {
   const description = reactDescriptions[genRandomInt(2)];
   return (
     <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImg} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
           {description} React concepts you will need for almost any app you are
@@ -21,13 +22,30 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li className="core-concept">
+      <img src={props.image} alt={props.title}/>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
-      <Header></Header>
-
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept title="Components" description="The core UI building block." image={compononetsImg} />
+            <CoreConcept title="Props" />
+            <CoreConcept />
+            <CoreConcept  />
+          </ul>
+        </section>
       </main>
     </div>
   );
