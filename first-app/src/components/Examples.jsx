10c47d6430dc9052b212cf081e74b9cfdad61cb2
Angelika Ervasti <angelika.ervasti@hotmail.fi>
2025-03-19 12:07:15 +0200

    Wrapped elements

--- first-app/src/components/Examples.jsx
+++ first-app/src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import TabButton from './TabButton.jsx';
 import { EXAMPLES } from '../data.js';
+import Section from './Section.jsx';
 
 export default function Examples() {
       const [ selectedTopic, setSelectedTopic ] = useState(null);
@@ -28,23 +29,22 @@ export default function Examples() {
       }
     
     return (
-        <section id="examples">
-          <h2>Examples</h2>
+        <Section title="Examples" id="examples">
           <menu>
-            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>
+            <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect('components')}>
               Components
             </TabButton>
-            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>
+            <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleSelect('jsx')}>
               JSX
             </TabButton>
-            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>
+            <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleSelect('props')}>
               Props
             </TabButton>
-            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>
+            <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleSelect('state')}>
               State
             </TabButton>
           </menu>
           {tabContent}
-        </section>
+        </Section>
       );
     }
